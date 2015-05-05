919fd8b9be21a3353da7baeb35b9fa586008eda4
mambax7 <mambax7@gmail.com>
2015-05-05 09:02:51 -0400

    Original files

---
+++ flex_3_red/manifesto.ini.php
@@ -0,0 +1,20 @@
+<?php
+/**
+
+[Manifesto]
+Name="flex_3_red"
+Depends=Legacy_RenderSystem
+Url="http://www.expiece.com/html/"
+Version="1.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=hi-debow @ EXTRAFFIC
+ScreenShot="screenshot.png"
+Description="3 columns flexible width tableless CSS layout style theme"
+
+License="Attribution-Noncommercial 2.1 Japan"
+
+*/
+?>
